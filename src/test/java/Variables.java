5da3ff39df1fce42660bd0efa74b2c130a968c09
Magdalena Dziedzic <magda.dziedzic1991@gmail.com>
2020-07-22 20:26:53 +0200

    new folder src

---
+++ src/test/java/Variables.java
@@ -0,0 +1,33 @@
+public class Variables {
+    double numbers  = 1.434;
+
+
+    public static void main(String args[]) {
+        int number = 1;
+        String name = "Madzia IKS DE";
+        boolean isTrue = true;
+
+        System.out.println(isTrue);
+
+        if (number-- < 2){
+            System.out.println("lower than two");
+        } else {
+            System.out.println("otóż nie ;)");
+        }
+
+    }
+    public void test1 () {
+        int number1 = 2;
+
+    }
+
+
+    public void test2() {
+        int number = 123;
+        String text = "wyjc stont";
+        boolean one = true;
+        boolean two = false;
+        System.out.println(numbers);
+
+    }
+}
